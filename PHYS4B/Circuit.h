465e25eb983b67fac6b35b48e16d10964c483e9d
jjfishtank <jjfishtank@gmail.com>
2022-03-12 23:07:49 -0800

    Implemented matrices calculations for modified nodal analysis linear systems.

--- PHYS4B/Circuit.h
+++ PHYS4B/Circuit.h
@@ -2,6 +2,7 @@
 // Electronic Circuit Simulation
 
 #include "Components.h"
+#include <eigen-3.4.0/Eigen/Dense>
 #include <unordered_map>
 #include <vector>
 #include <fstream>
@@ -16,6 +17,7 @@ public:
   Circuit(std::ifstream& fin);
   ~Circuit() {}
 
+  //Eigen::MatrixXd SolveCircuit();
   std::string toString();
 
   size_t component_count() const { return components_.size(); }
@@ -23,18 +25,28 @@ public:
   size_t current_count() const { return current_count_; }
   size_t resistor_count() const { return resistor_count_; }
   size_t nodes_count() const { return nodes_.size(); }
+  Eigen::MatrixXd A_matrix() const { return A_; }
+  Eigen::MatrixXd b_matrix() const { return b_; }
+
   const std::unordered_map<std::string, size_t>& nodes() const { return nodes_; }
 
 private:
+
+  void CalculateMatrices();
+
   std::vector<Component> components_;
   std::unordered_map<std::string, size_t> nodes_;
-  size_t voltage_count_;
-  size_t current_count_;
-  size_t resistor_count_;
+  size_t voltage_count_, current_count_, resistor_count_, conductor_count_, inductor_count_;
+  size_t matrix_size_;
+  Eigen::MatrixXd A_, b_;
+
 };
 
 // Parses circuit file and creates component vector
-Circuit::Circuit(std::ifstream& fin) : voltage_count_(0), current_count_(0), resistor_count_(0) {
+Circuit::Circuit(std::ifstream& fin)
+  : voltage_count_(0), current_count_(0), resistor_count_(0)
+  , conductor_count_(0), inductor_count_(0), matrix_size_(0) {
+
   char type = 0;
   size_t count = 0;
   std::string p_node, n_node;
@@ -61,14 +73,20 @@ Circuit::Circuit(std::ifstream& fin) : voltage_count_(0), current_count_(0), res
       istream >> value;
 
       switch (type) {
-        case 'V':
-          voltage_count_++;
+        case 'C':
+          conductor_count_++;
+          break;
+        case 'I':
+          current_count_++;
+          break;
+        case 'L':
+          inductor_count_++;
           break;
         case 'R':
           resistor_count_++;
           break;
-        case 'I':
-          current_count_++;
+        case 'V':
+          voltage_count_++;
           break;
         default:
           break;
@@ -79,13 +97,93 @@ Circuit::Circuit(std::ifstream& fin) : voltage_count_(0), current_count_(0), res
   }
   fin.close();
 
+  // default node 0
   nodes_.insert({ "0", 0 });
-
+  // map node names to integers
   for (auto& i : components_) {
     nodes_.insert({ i.p_node() , nodes_.size() });
-    nodes_.insert({ i.p_node() , nodes_.size() });
+    nodes_.insert({ i.n_node() , nodes_.size() });
+  }
+
+  // Populate A, b matrices for MNA linear system
+  CalculateMatrices();
+}
+
+void Circuit::CalculateMatrices() {
+  size_t g2_count = 0, g2_index = 0;
+  g2_count = voltage_count_ + inductor_count_;
+  matrix_size_ = nodes_.size() + g2_count - 1;
+  g2_index = matrix_size_ - g2_count;
+
+  A_.resize(matrix_size_, matrix_size_);
+  A_.fill(0.0);
+  b_.resize(1, matrix_size_);
+  b_.fill(0.0);
+
+  for (auto& i : components_) {
+    auto search = nodes_.find(i.p_node());
+    size_t p_node = search->second;
+    search = nodes_.find(i.n_node());
+    size_t n_node = search->second;
+    double value = i.value();
+
+    switch (i.type()) {
+      case 'C':
+        break;
+      case 'I':
+        // signed currents in b matrix
+        if (p_node != 0)
+          b_(p_node - 1) = b_(p_node - 1) - value;
+        if (n_node != 0)
+          b_(n_node - 1) = b_(n_node - 1) + value;
+        break;
+      case 'L':
+        if (p_node != 0) {
+          A_(p_node - 1, g2_index)++;
+          A_(g2_index, p_node - 1)++;
+        }
+        if (n_node != 0) {
+          A_(n_node - 1, g2_index)--;
+          A_(g2_index, n_node - 1)--;
+        }
+
+        b_(g2_index) = 0.0;
+        g2_index++;
+        break;
+      case 'R':
+        // G-Matrix A
+        if (p_node != 0)
+          A_(p_node - 1, p_node - 1) = A_(p_node - 1, p_node - 1) + 1.0 / value;
+        if (n_node != 0)
+          A_(n_node - 1, n_node - 1) = A_(n_node - 1, n_node - 1) + 1.0 / value;
+        // Mutual conductance
+        if (p_node != 0 && n_node != 0) {
+          A_(p_node - 1, n_node - 1) = A_(p_node - 1, n_node - 1) - 1.0 / value;
+          A_(n_node - 1, p_node - 1) = A_(n_node - 1, p_node - 1) - 1.0 / value;
+        }
+        break;
+      case 'V':
+        if (p_node != 0) {
+          A_(p_node - 1, g2_index)++;
+          A_(g2_index, p_node - 1)++;
+        }
+        if (n_node != 0) {
+          A_(n_node - 1, g2_index)--;
+          A_(g2_index, n_node - 1)--;
+        }
+
+        b_(g2_index) = value;
+        g2_index++;
+        break;
+      default:
+        break;
+    }
   }
 }
+
+/*Eigen::MatrixXd Circuit::SolveCircuit() {
+
+}*/
 
 std::string Circuit::toString() {
   std::string result;
---
+++ PHYS4B/Kirchhoff.cpp
@@ -0,0 +1,125 @@
+﻿// Jeremy Renati
+// Kirchhoff
+
+#include "Circuit.h"
+#include <iostream>
+
+
+using namespace std;
+
+void ManualMeshAnalysis() {
+  /*
+    ┌ R1 ┬ R2 ┬ R3 ┐
+    │    R4    R5   R6
+    └ V1 ┴────┴ V2 ┘
+     -  +      -  +
+  */
+
+  double R1 = 5;
+  double R2 = 10;
+  double R3 = 3;
+  double R4 = 7;
+  double R5 = 4;
+  double R6 = 1;
+  double V1 = 1;
+  double V2 = 3;
+
+  /*
+  Mesh 1: V1-R1-R4 Loop
+  Mesh 2: R4-R2-R5 Loop
+  Mesh 3: V2-R5-R3-R6 Loop
+
+  Mesh Currents (all clockwise)
+  I1 for Mesh 1
+  I2 for Mesh 2
+  I3 for Mesh 3
+  */
+
+  Eigen::Matrix<double, 3, 3> A {
+    { R1 + R4, -R4, 0 },
+    { -R4, R2 + R4 + R5, -R5 },
+    { 0, -R5, R3 + R5 + R6 }
+  };
+  Eigen::Matrix<double, 3, 1> B { -V1, 0 , -V2 };
+
+  cout << "Matrix A:\n" << A << endl;
+  cout << "Matrix B:\n" << B << endl;
+
+  cout << "Computing System...\n" << endl;
+  // Eigen linear solver
+  Eigen::Matrix<double, 1, 3> X = A.colPivHouseholderQr().solve(B);
+
+  cout << "Solution: \n";
+  cout << "I1 = " << X[0] << ", I2 = " << X[1] << ", I3 = " << X[2] << endl;
+}
+
+void ManualNodalAnalysis() {
+  /*
+  Electronic Circuit is
+    ┌───┬ R3 ┬───┐
+  ↑ I1  R1   R2  I2 ↓
+    └───┴─┬──┴───┘
+          ⏚
+  */
+
+  double R1 = 4;
+  double R2 = 10;
+  double R3 = 6;
+  double I1 = 1;
+  double I2 = 2;
+
+  /*
+  Node 0: Connecting I1-R1-R2-I2 (node at the bottom)
+  Node A: Connecting I1-R1-R3    (upper left node)
+  Node B: Connecting I2-R2-R3    (upper right node)
+  */
+
+  Eigen::Matrix<double, 2, 2> A{
+    { 1 / R1 + 1 / R3, -(1 / R3) },
+    { -(1 / R3), 1 / R2 + 1 / R3 },
+  };
+  Eigen::Matrix<double, 2, 1> B{ I1, -I2 };
+
+
+  cout << "Matrix A:\n" << A << endl;
+  cout << "Matrix B:\n" << B << endl;
+
+  cout << "Computing System...\n" << endl;
+  // Eigen linear solver
+  Eigen::Matrix<double, 1, 2> X = A.colPivHouseholderQr().solve(B);
+
+  cout << "Solution: \n";
+  cout << "Va = " << X[0] << ", Vb = " << X[1] << endl;
+}
+
+int main() {
+  ifstream fin("circuit.txt");
+  Circuit* circuit_1 = new Circuit(fin);
+  cout << "Circuit\n";
+  cout << circuit_1->toString();
+
+  cout << "\nCircuit Info\n";
+  cout << "Component count: " << circuit_1->component_count() << endl;
+  cout << "Voltage count: " << circuit_1->voltage_count() << endl;
+  cout << "Current count: " << circuit_1->current_count() << endl;
+  cout << "Resistor count: " << circuit_1->resistor_count() << endl;
+  cout << "Node count: " << circuit_1->nodes_count() << endl << endl;
+  cout << "Matrix A:\n" << circuit_1->A_matrix() << endl;
+  cout << "Matrix B:\n" << circuit_1->b_matrix() << endl;
+  
+  /*
+  cout << "\nNodes list:\n";
+  for (auto i : circuit_1->nodes()) {
+    cout << '\"' << i.first << "\" : " << i.second << endl;
+  }
+  */
+
+  /* manual nodal/mesh analysis
+  cout << endl;
+  ManualMeshAnalysis();
+  cout << endl;
+  ManualNodalAnalysis();
+  */
+
+  return 0;
+}
